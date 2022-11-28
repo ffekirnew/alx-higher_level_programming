28828c31ec431c5d500c48aff2dc9ff6308487f6
ffekirnew <ffekirnew0808@gmail.com>
2022-11-28 08:27:50 +0300

    started on task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdio.h>
+
+
+/**
+ * check_cycle - will check if there is a cycle in a linked list
+ * @list: the linked list to be checked
+ *
+ * Return: 0 if there's no cycle, 1 if there is
+ */
+int check_cycle(listint_t *list)
+{
+    listint_t *turtle = list;
+
+    free(turtle);
+    return (0);
+}
