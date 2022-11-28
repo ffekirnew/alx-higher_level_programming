352e63e04d80624070b93a93db484acf6d23c1e3
ffekirnew <ffekirnew0808@gmail.com>
2022-11-28 22:52:37 +0300

    added the files for task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -3,15 +3,27 @@
 
 
 /**
- * check_cycle - will check if there is a cycle in a linked list
+ * check_cycle - will check if there is a cycle in a linked hare
  * @list: the linked list to be checked
  *
  * Return: 0 if there's no cycle, 1 if there is
  */
 int check_cycle(listint_t *list)
 {
-    listint_t *turtle = list;
+    listint_t *turtle = list->next;
+    listint_t *hare;
 
-    free(turtle);
+
+    if (list->next == NULL)
+        return (0);
+
+    hare = list->next->next;
+    while ((turtle->next != NULL) && (hare->next->next != NULL))
+    {
+        if (turtle->n == hare->n)
+            return (1); 
+        turtle = turtle->next;
+        hare = hare->next->next;
+    }
     return (0);
 }
