df2cd22d2a86b84bb2ab2f402a1a2daabd0caf65
ffekirnew <ffekirnew0808@gmail.com>
2022-12-01 20:50:54 +0300

    added the file for task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    for i, arg in enumerate(sys.argv):
+    no_of_arguments = len(argv) - 1
+    if not no_of_arguments:
+        print("0 arguments.")
+    elif no_of_arguments == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(no_of_arguments))
+    for i, arg in enumerate(argv):
         if not i:
             continue
         print("{}: {}".format(i, arg))
