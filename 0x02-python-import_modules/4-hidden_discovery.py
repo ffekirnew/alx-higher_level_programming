3e53b80a49af41a8e72f66603aba5f7fdc838fec
ffekirnew <ffekirnew0808@gmail.com>
2022-12-01 20:35:21 +0300

    updated the file for task 4 such that it meets the requirements

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,8 @@
+#!/usr/bin/python3
 import hidden_4
 
 if __name__ == "__main__":
-	dir(hidden_4)
+    for name in dir(hidden_4):
+        if name[0] + name[1] == "__":
+            continue
+        print(name)
