4a4f857814563eb6c398fb9b2b2f124a79af07ef
ffekirnew <ffekirnew0808@gmail.com>
2022-12-04 22:09:22 +0300

    fixed a syntax error

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,6 +4,6 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     a = len(tuple_a)
     b = len(tuple_b)
-    answer_1 = t(uple_a[0] if a > 0 else 0) + (tuple_b[0] if b > 0 else 0)
+    answer_1 = (tuple_a[0] if a > 0 else 0) + (tuple_b[0] if b > 0 else 0)
     answer_2 = (tuple_a[1] if a > 1 else 0) + (tuple_b[1] if b > 1 else 0)
     return (answer_1, answer_2)
