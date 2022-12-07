f4dcede29fa8b70f040dbf0c330f59112b5dc2bc
ffekirnew <ffekirnew0808@gmail.com>
2022-12-07 22:20:16 +0300

    fixed a lambda function to be more accurate

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,6 +3,6 @@
 
 def print_sorted_dictionary(a_dictionary):
     a_list = [[key, value] for key, value in a_dictionary.items()]
-    a_list.sort(key=lambda x: x[0])
+    a_list.sort(key=lambda x: ord(x[0][0]))
     for item in a_list:
         print(item[0] + ": ", item[1])
