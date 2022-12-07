24790727a128abe0c9d75e47832ed27531734fe5
ffekirnew <ffekirnew0808@gmail.com>
2022-12-07 23:34:10 +0300

    added the solution to optional task 102

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def complex_delete(a_dictionary, value):
+    if not a_dictionary:
+        return None
+    new_dictionary = a_dictionary.copy()
+    for key, key_value in new_dictionary.items():
+        if key_value == value:
+            del new_dictionary[key]
+    return new_dictionary
