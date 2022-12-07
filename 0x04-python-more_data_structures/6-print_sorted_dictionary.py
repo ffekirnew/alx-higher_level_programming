d6865a222a459f007bb158656abd76b9c265a98c
ffekirnew <ffekirnew0808@gmail.com>
2022-12-07 23:21:28 +0300

    fixed formating issue

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -5,4 +5,4 @@ def print_sorted_dictionary(a_dictionary):
     a_list = [[key, value] for key, value in a_dictionary.items()]
     a_list.sort(key=lambda x: ord(x[0][0]))
     for item in a_list:
-        print(item[0] + ": ", item[1])
+        print(item[0] + ":", item[1])
