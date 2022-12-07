a86cebf26e2eae139291851a74afbe870a9cf94e
ffekirnew <ffekirnew0808@gmail.com>
2022-12-07 22:19:11 +0300

    added the file for task 6

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def print_sorted_dictionary(a_dictionary):
+    a_list = [[key, value] for key, value in a_dictionary.items()]
+    a_list.sort(key=lambda x: x[0])
+    for item in a_list:
+        print(item[0] + ": ", item[1])
