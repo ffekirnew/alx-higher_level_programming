a90bb2ff8f28d66aaf277b0621753eb69d5e1bcf
ffekirnew <ffekirnew0808@gmail.com>
2022-12-09 21:28:07 +0300

    added all loose files

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,6 +2,8 @@
 
 
 def print_sorted_dictionary(a_dictionary):
+    if not a_dictionary:
+        return None
     a_list = [[key, value] for key, value in a_dictionary.items()]
     a_list.sort(key=lambda x: ord(x[0][0]))
     for item in a_list:
