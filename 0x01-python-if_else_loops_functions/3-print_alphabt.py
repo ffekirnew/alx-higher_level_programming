6bfbb454733c70579a565a3a2c8f96c752c0c90b
ffekirnew <ffekirnew0808@gmail.com>
2022-12-14 07:34:19 +0300

    changed the print style to use format

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 for ascii_index in range(97, 123):
     if (ascii_index == 101 or ascii_index == 113):
         continue
-    print(f"{chr(ascii_index)}", end="")
+    print("{}".format(chr(ascii_index)), end="")
