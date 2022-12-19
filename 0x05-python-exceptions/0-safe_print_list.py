05e309eb9fce77f2f17dc24609aa8f54b05087ce
ffekirnew <ffekirnew0808@gmail.com>
2022-12-19 11:24:59 +0300

    fixed a semantic error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,6 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[count], end='')
             count += 1
         except IndexError:
-            print()
             break
+    print()
     return count
