c556d3fbd36ac1b10f4107eb819d1985fec43db5
ffekirnew <ffekirnew0808@gmail.com>
2022-12-20 21:44:44 +0300

    added the file for task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module contains the class square
+"""
+
+
+class Square:
+    '''This is class defines a squre with the given size
+    '''
+    def __init__(self, size):
+        self.__size = size
