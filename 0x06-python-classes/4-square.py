a84db485b97623f964cecbfb37e2e477f26b7545
ffekirnew <ffekirnew0808@gmail.com>
2022-12-21 05:09:42 +0300

    added the file for task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""This module contains the class square
+"""
+
+
+class Square:
+    '''This is class defines a squre with the given size
+    '''
+    def __init__(self, size=0):
+        '''This constructor will initialize the class with the given
+        size or will give it the default value 0.
+        Args:
+            size (int): The size for the square
+        '''
+        try:
+            size = int(size)
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            self.size = size
+        except TypeError as e:
+            raise e("size must be an integer")
+
+    def area(self):
+        '''This method will return the current area of the
+        given square instance
+        '''
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        '''Getter for size
+        '''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        ''' sets a value for the private variable size
+        '''
+        try:
+            value = int(value)
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = value
+        except TypeError as e:
+            raise e("size must be an integer")
