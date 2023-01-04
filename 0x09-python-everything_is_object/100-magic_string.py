2cff48cabceab4c63870893122bbc71dce6b1a81
ffekirnew <ffekirnew0808@gmail.com>
2023-01-04 04:42:09 +0300

    fixed line length

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
     for i in range(11):
-        for j in range(i):
-            print("BestSchool", end=', ' if j < i - 1 else '\n')
+        for j in range(i): print("BestSchool", end=', ' if j < i - 1 else '\n')
