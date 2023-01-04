fbaef744bc51eb9bf8e4331dfc639945a31653a3
ffekirnew <ffekirnew0808@gmail.com>
2023-01-04 04:47:35 +0300

    fixed by reducing one line from the code

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    for i in range(11):
-        for j in range(i): print("BestSchool", end=', ' if j < i - 1 else '\n')
+    for i in range(10):
+        print("BestSchool, " * (i) + "BestSchool")
