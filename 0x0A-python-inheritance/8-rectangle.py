6ab95bb552ae83aabb01a8355d0d8e7152d2c001
ffekirnew <ffekirnew0808@gmail.com>
2023-01-09 19:22:47 +0300

    added the file for task 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Define a rectangle class."""
+
+    def __init__(self, width, height):
+        """Construct a rectangle with width and height.
+        Parameters:
+            width: the width of the rectangle.
+            height: the height of the rectangle.
+        """
+        super().__init__()
+        self.__width = self.integer_validator('width', width)
+        self.__height = self.integer_validator('height', height)
