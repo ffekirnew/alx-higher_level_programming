0ba606c18a86bf34f0f7626855643e1a08420261
ffekirnew <ffekirnew0808@gmail.com>
2023-01-09 17:18:04 +0300

    added the file for task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Contain empty class definition for BaseGeometry."""
+
+
+class BaseGeometry:
+    """Define the class BaseGeometry."""
+
+    def area(self):
+        raise Exception('area() is not implemented')
