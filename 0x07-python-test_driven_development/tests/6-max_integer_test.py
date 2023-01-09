2e13501223c8b38ae8bdab1a5619d18bc95f6862
ffekirnew <ffekirnew0808@gmail.com>
2023-01-09 17:02:34 +0300

    added the file for task 6

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,19 +1,36 @@
 #!/usr/bin/python3
-"""Unittest for max_integer([..])
-"""
 import unittest
-max_integer = __import__('6-max_integer').max_integer
-
 
 class TestMaxInteger(unittest.TestCase):
-    def test_max_integer(self):
-        self.assertEqual(max_integer([1, 2, 3]), 3)
-        self.assertEqual(max_integer([-1, -2, -3]), -1)
-        self.assertEqual(max_integer([]), None)
-        self.assertEqual(max_integer([1]), 1)
+    
+    def test_normal_execution(self):
+        test_list = [1, 2, 3, 5, -10]
+        self.assertEqual(max_integer(test_list), 5)
 
-    def test_values(self):
-        self.assertRaises(TypeError, max_integer, [3.5])
-        self.assertRaises(TypeError, max_integer, ["hello"])
-        self.assertRaises(TypeError, max_integer, [TestMaxInteger()])
-        self.assertRaises(TypeError, max_integer, [True])
+    def test_empty_list(self):
+        empty_list = []
+        self.assertEqual(max_integer(empty_list), None)
+        
+    def test_one_element(self):
+        one_element = [500]
+        self.assertEqual(max_integer(one_element), 500)
+
+    def test_bigger_first(self):
+        bigger_first = [100, 10]
+        self.assertEqual(max_integer(bigger_first), 100)
+
+    def test_negative_numbers(self):
+        negative = [-20, -10, -15]
+        self.assertEqual(max_integer(negative), -10)
+
+    def test_duplicates(self):
+        duplicates = [2, 2, 2]
+        self.assertEqual(max_integer(duplicates), 2)
+
+    def test_mixed_list(self):
+        mixed_list = [1, "hello", [], -2]
+        with self.assertRaises(TypeError):
+            max_integer(mixed_list)
+            
+if __name__ == '__main__': 
+    unittest.main()
