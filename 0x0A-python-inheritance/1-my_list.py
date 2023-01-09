662a9f2185bc84c15877bcf394f5ede2c4b17f0c
ffekirnew <ffekirnew0808@gmail.com>
2023-01-09 16:30:59 +0300

    fixed unnecessary class declaration

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,10 +5,6 @@
 class MyList(list):
     """Add other functionalities to builtin list."""
 
-    def __init__(self, *arg):
-        """Construct an instance of the class."""
-        list.__init__(self, *arg)
-
     def print_sorted(self):
         """Print sorted list (ascending sort)."""
         sorted_list = sorted(self)
