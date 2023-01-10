cabb5bcb02c3df1f604e4217133e8a643ddad5fc
ffekirnew <ffekirnew0808@gmail.com>
2023-01-10 22:49:24 +0300

    fixed an issue

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,4 @@
 def read_file(filename=""):
     """Read the file and print the contents."""
     with open(filename, mode='r', encoding='UTF8') as file:
-        data = file.read()
-    print(data)
+        print(file.read())
