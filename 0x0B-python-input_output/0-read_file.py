d977ea2909163279b9852624eab66f6b4a40063e
ffekirnew <ffekirnew0808@gmail.com>
2023-01-10 21:09:29 +0300

    added the file for task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define function read_file"""
+
+
+def read_file(filename=""):
+    """Read the file and print the contents."""
+    with open(filename, mode='r', encoding='UTF8') as file:
+        data = file.read()
+    print(data)
